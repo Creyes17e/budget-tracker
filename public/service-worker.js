b33faf56ae243a113db1b60bf52d595fef4df621
Carina Reyes <cereyes1792@gmail.com>
2020-08-15 15:06:54 -0500

    renamed css file to style.css and updated other files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,11 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/styles.css",
+  "/style.css",
   "/index.js",
   "/manifest.webmanifest",
-  "/icons/icon-192x192",
-  "/icons/icon-512x512",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";

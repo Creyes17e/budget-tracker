0705d84311ccda78bfc4e10b5e2db278dd3c916b
Carina Reyes <cereyes1792@gmail.com>
2020-08-15 14:12:47 -0500

    updated server file for future deployment

--- server.js
+++ server.js
@@ -15,9 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+let uri = "mongodb://localhost/budget_db";
+if (process.env.NODE_ENV === "production") {
+  uri = process.env.MONGODB_URI;
+}
+mongoose.connect(uri, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
 });
 
 // routes
